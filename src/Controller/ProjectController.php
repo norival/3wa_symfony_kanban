090c2e7a9549e801a0dce7da77ee96f9073daca5
Xavier Laviron <xavier@norival.dev>
2021-05-31 18:08:04 +0200

    adds templates for projects

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Project;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,10 +16,21 @@ class ProjectController extends AbstractController
         /** @var \App\Entity\User $user */
         $user = $this->getUser();
 
-        $projects = $user->getUserProjects();
+        $projects = $user->getProjects();
+        $userProjects = $user->getUserProjects();
 
         return $this->render('project/index.html.twig', [
             'projects' => $projects,
+            'userProjects' => $userProjects,
+        ]);
+    }
+
+    #[Route('/project/{id}', name: 'project_show')]
+    #[IsGranted('ROLE_USER')]
+    public function show(Project $project): Response
+    {
+        return $this->render('project/show.html.twig', [
+            'project' => $project,
         ]);
     }
 }

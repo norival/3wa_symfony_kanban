61f49b68ac4161acaaf4834a3119a903d51662e6
Xavier Laviron <xavier@norival.dev>
2021-05-31 16:06:11 +0200

    adds authenticator

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -58,21 +58,6 @@ class SecurityController extends AbstractController
         ]);
     }
 
-    #[Route('/login', name: 'security_login')]
-    public function login(AuthenticationUtils $authenticationUtils): Response
-    {
-        // get the login error if there is one
-        $error = $authenticationUtils->getLastAuthenticationError();
-
-        // last username entered by the user
-        $lastUsername = $authenticationUtils->getLastUsername();
-
-        return $this->render('security/login.html.twig', [
-            'last_username' => $lastUsername,
-            'error'         => $error,
-        ]);
-    }
-
     #[Route('/logout', name: 'security_logout')]
     public function logout(): void
     {
@@ -91,5 +76,22 @@ class SecurityController extends AbstractController
 
         return $this->render('security/activation.html.twig', [
         ]);
+    }
+
+    /**
+     * #Route('/login', name: 'app_login')
+     */
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        // if ($this->getUser()) {
+        //     return $this->redirectToRoute('target_path');
+        // }
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
 }

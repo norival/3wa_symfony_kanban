19fa8538054e3a31f193c6b80ef79101adf068fe
Xavier Laviron <xavier@norival.dev>
2021-05-31 14:03:02 +0200

    adds registration form

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\RegistrationType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,8 +14,11 @@ class SecurityController extends AbstractController
     #[Route('/register', name: 'security_register')]
     public function index(): Response
     {
-        return $this->render('security/index.html.twig', [
-            'controller_name' => 'SecurityController',
+        $user = new User();
+        $form = $this->createForm(RegistrationType::class, $user);
+
+        return $this->render('security/register.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 

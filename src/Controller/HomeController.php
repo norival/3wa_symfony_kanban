e5ce9e952b4786ed5814c38b9e9cbda037628146
Xavier Laviron <xavier@norival.dev>
2021-06-01 12:45:44 +0200

    use pagination

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,19 +5,34 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Form\RegisterType;
 use App\Repository\ProjectRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
+    public function __construct(
+        private EntityManagerInterface $em,
+        private PaginatorInterface $paginator
+    ) { }
+
     #[Route('/', name: 'home')]
-    public function index(ProjectRepository $projectRepository): Response
+    public function index(Request $request): Response
     {
-        $projects = $projectRepository->findAll();
+        $dql = 'SELECT a FROM App\Entity\Project a';
+        $query = $this->em->createQuery($dql);
+
+        $pagination = $this->paginator->paginate(
+            $query,
+            $request->query->getInt('page', 1),
+            5
+        );
 
         return $this->render('home/index.html.twig', [
-            'projects' => $projects,
+            'pagination' => $pagination,
         ]);
     }
 }

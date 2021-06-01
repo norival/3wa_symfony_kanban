96c9ad79bb5bebba171cc03f41daa9c169b8e1f7
Xavier Laviron <xavier@norival.dev>
2021-06-01 16:35:57 +0200

    adds stuff for tags

--- src/Controller/TagController.php
+++ src/Controller/TagController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Tag;
+use App\Form\TagType;
+use App\Repository\TagRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +12,27 @@ use Symfony\Component\Routing\Annotation\Route;
 class TagController extends AbstractController
 {
     #[Route('/tag', name: 'tag')]
-    public function index(): Response
+    public function index(TagRepository $tagRepository): Response
     {
+        $tags = $tagRepository->findAll();
+
         return $this->render('tag/index.html.twig', [
-            'controller_name' => 'TagController',
+            'tags' => $tags,
+        ]);
+    }
+
+    #[Route('/tag/new', name: 'tag_new')]
+    #[Route('/tag/edit/{id}', name: 'tag_edit')]
+    public function edit(Tag $tag = null): Response
+    {
+        if (!$tag) {
+            $tag = new Tag();
+        }
+
+        $form = $this->createForm(TagType::class, $tag);
+
+        return $this->render('tag/edit.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }

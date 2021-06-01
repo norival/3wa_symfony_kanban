64eea82369c793efba14b1f1abaa7b3e7dbb3725
Xavier Laviron <xavier@norival.dev>
2021-06-01 14:59:17 +0200

    adds account edit route

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,13 +2,22 @@
 
 namespace App\Controller;
 
+use App\Form\UserType;
+use App\Service\FileUploader;
+use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AccountController extends AbstractController
 {
+    public function __construct(
+        private EntityManagerInterface $em,
+        private FileUploader $fileUploader
+    ) { }
+
     #[Route('/account', name: 'account')]
     #[IsGranted('ROLE_USER')]
     public function index(): Response
@@ -17,6 +26,33 @@ class AccountController extends AbstractController
         $user = $this->getUser();
 
         return $this->render('account/index.html.twig', [
+            'user' => $user,
+        ]);
+    }
+
+    #[Route('/account/edit', name: 'account_edit')]
+    public function edit(Request $request): Response
+    {
+        /** @var \App\Entity\User $user */
+        $user = $this->getUser();
+
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $profilePicture = $form->get('profilePicture')->getData();
+            if ($profilePicture) {
+                $filename = $this->fileUploader->upload($profilePicture);
+                $user->setProfilePicture($filename);
+            }
+
+            $this->em->flush();
+
+            return $this->redirectToRoute('account');
+        }
+
+        return $this->render('account/edit.html.twig', [
+            'form' => $form->createView(),
             'user' => $user,
         ]);
     }

144ad3c3bd70eeecc164595b8c7a66b61bad8d05
Xavier Laviron <xavier@norival.dev>
2021-06-01 19:34:22 +0200

    add stuff

--- assets/app.js
+++ assets/app.js
@@ -13,3 +13,6 @@ import './styles/global.scss';
 import './bootstrap';
 
 require('bootstrap');
+
+require('@fortawesome/fontawesome-free/css/all.min.css');
+require('@fortawesome/fontawesome-free/js/all.js');

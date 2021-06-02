f9cdec514fd5673ac7f16ea4a12637a2bd3e1040
Xavier Laviron <xavier@norival.dev>
2021-06-02 15:46:35 +0200

    render recent projects in menu

--- src/Controller/MenuController.php
+++ src/Controller/MenuController.php
@@ -2,17 +2,19 @@
 
 namespace App\Controller;
 
+use App\Repository\ProjectRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MenuController extends AbstractController
 {
-    #[Route('/menu', name: 'menu')]
-    public function index(): Response
+    public function getRecentProjects(ProjectRepository $projectRepository): Response
     {
-        return $this->render('menu/index.html.twig', [
-            'controller_name' => 'MenuController',
+        $projects = $projectRepository->findBy([], ['createdAt' => 'DESC'], 3);
+
+        return $this->render('menu/_recent_projects.html.twig', [
+            'projects' => 'MenuController',
         ]);
     }
 }
